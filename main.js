48d03a5185c6a1cfe4436720ce46534d4cf619f4
skozi-dev <n10895647@qut.edu.au>
2024-06-16 20:09:38 +1000

    Impemented randomised computer choice

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+//=================================
+// ROCK PAPER SCISSOR
+// ================================
+
+// Create a function to randomly generate rock paper or scissor
+let getComputerChoice = () => {
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    console.log("rock");
+  } else if (randomNumber === 1) {
+    console.log("paper");
+  } else {
+    console.log("scissor");
+  }
+};
+
+getComputerChoice();
+//
