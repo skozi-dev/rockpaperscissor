eb46f9b511729acc6bfc394eba530190b4bb6bb4
skozi-dev <n10895647@qut.edu.au>
2024-06-17 10:18:50 +1000

    Added playGame, playRound, gameOver functions - game works but need to implement an option to quit early.

--- main.js
+++ main.js
@@ -1,18 +1,101 @@
 //=================================
 // ROCK PAPER SCISSOR
-// ================================
+//=================================
+
+// Keep Track of Score
+let humanScore = 0;
+let computerScore = 0;
 
 // Create a function to randomly generate rock paper or scissor
 let getComputerChoice = () => {
   let randomNumber = Math.floor(Math.random() * 3);
+  let computerSelection = "";
+
   if (randomNumber === 0) {
-    console.log("rock");
+    computerSelection = "rock";
   } else if (randomNumber === 1) {
-    console.log("paper");
+    computerSelection = "paper";
   } else {
-    console.log("scissor");
+    computerSelection = "scissor";
   }
+
+  return computerSelection;
 };
 
-getComputerChoice();
-//
+// Write a function to get users input of rock paper or scissor
+let getHumanChoice = () => {
+  let getUserInput = prompt("Would you like to play PAPER(P), ROCK(R), or SCISSOR(S)?");
+  let humanSelection = "";
+
+  if (getUserInput.toLowerCase() === "p" || getUserInput.toLowerCase() === "paper") {
+    humanSelection = "paper";
+  } else if (getUserInput.toLowerCase() === "r" || getUserInput.toLowerCase() === "rock") {
+    humanSelection = "rock";
+  } else if (getUserInput.toLowerCase() === "s" || getUserInput.toLowerCase() === "scissor") {
+    humanSelection = "scissor";
+  } else {
+    alert("Please choose a valid move!");
+    getHumanChoice();
+  }
+
+  return humanSelection;
+};
+
+// Write the logic to play a single round
+function playRound(humanChoice, computerChoice) {
+  humanChoice = getHumanChoice();
+  computerChoice = getComputerChoice();
+
+  // Compare humanChoice vs computerChoice to get result
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("Paper beats Rock! You lose!");
+    computerScore += 1;
+  } else if (humanChoice === "paper" && computerChoice === "scissor") {
+    console.log("Scissor beats Paper! You lose!");
+    computerScore += 1;
+  } else if (humanChoice === "scissor" && computerChoice === "rock") {
+    console.log("Rock beats Scissor! You lose!");
+    computerScore += 1;
+  } else if (humanChoice === computerChoice) {
+    console.log(`${humanChoice} and ${computerChoice} - it's a stalemate. No Score.`);
+  } else {
+    console.log(`${humanChoice} beats ${computerChoice}! You Win!`);
+    humanScore += 1;
+  }
+
+  console.log("Your Score: " + humanScore);
+  console.log("Computer Score: " + computerScore);
+}
+
+//  End the game and display scores
+function gameOver() {
+  if (humanScore > computerScore) {
+    alert(`Game Over. You Win!\n Score: ${humanScore}\n Opponent: ${computerScore}`);
+  } else {
+    alert(`Game Over. You Lose!\n Score: ${humanScore}\n Opponent: ${computerScore}`);
+  }
+}
+
+// Write a function to play a game of 5 rounds, give the player an option to quit,
+// display score to console after each round
+function playGame() {
+  roundsPlayed = 0;
+
+  while (roundsPlayed < 5) {
+    playRound();
+    roundsPlayed += 1;
+  }
+  gameOver();
+}
+
+playGame();
+
+// const continuePrompt = () => {
+//   prompt("Would You Like to continue (y/n)?");
+
+//   if (continuePrompt === "y") {
+//     playRound();
+//   } else {
+//     gameOver();
+//   }
+// };
