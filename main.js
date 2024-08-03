625c05eb8f763585490f80dd15f3f996bb5ee9f6
skozi-dev <n10895647@qut.edu.au>
2024-08-03 14:19:11 +1000

    Buttons implement for player interaction. Dom manipulated to display buttons and results. No score board shown - to be implemented. TO FIX - result of tie and win shows all caps

--- main.js
+++ main.js
@@ -5,11 +5,31 @@
 //  List Possible Choices
 const choices = ["rock", "paper", "scissor"];
 
+// Create 3 new buttons to represent paper rock scissor
+const rockButton = document.createElement("button");
+const paperButton = document.createElement("button");
+const scissorButton = document.createElement("button");
+rockButton.textContent = "rock";
+paperButton.textContent = "paper";
+scissorButton.textContent = "scissor";
+
+// Append buttons to body of html
+const buttons = [rockButton, paperButton, scissorButton];
+const body = document.querySelector("body");
+buttons.forEach((button) => {
+  body.appendChild(button);
+});
+
+// Create display board
+const resultDiv = document.createElement("div");
+const result = document.createElement("p");
+const currentScore = document.createElement("h2");
+
 // Keep track of scores
 let humanScore = 0;
 let computerScore = 0;
 
-// Generate randomised computer play
+// Generate randomized computer play
 const getComputerChoice = () => {
   let randomNum = Math.floor(Math.random() * choices.length);
   let computerChoice = choices[randomNum];
@@ -17,48 +37,65 @@ const getComputerChoice = () => {
   return computerChoice;
 };
 
-// Get players choice of throw
-const getHumanChoice = () => {
-  let humanChoice = prompt("Choose your throw! Paper, Rock, Scissor?").toLowerCase();
-
-  return humanChoice;
-};
-
 // Logic for one round of gameplay
-const playRound = (humanChoice, computerChoice) => {
-  if (humanChoice === "rock" && computerChoice === "paper") {
-    console.log("Paper beats Rock! You lose!");
+const playRound = (humanSelection, computerSelection) => {
+  body.appendChild(resultDiv);
+  resultDiv.appendChild(result);
+
+  if (humanSelection === "rock" && computerSelection === "paper") {
+    result.textContent = "Paper beats Rock! You lose!";
     computerScore += 1;
-  } else if (humanChoice === "paper" && computerChoice === "scissor") {
-    console.log("Scissor beats Paper! You lose!");
+  } else if (humanSelection === "paper" && computerSelection === "scissor") {
+    result.textContent = "Scissor beats Paper! You lose!";
     computerScore += 1;
-  } else if (humanChoice === "scissor" && computerChoice === "rock") {
-    console.log("Rock beats Scissor! You lose!");
+  } else if (humanSelection === "scissor" && computerSelection === "rock") {
+    result.textContent = "Rock beats Scissor! You lose!";
     computerScore += 1;
-  } else if (humanChoice === computerChoice) {
-    console.log(`${humanChoice} and ${computerChoice} - it's a stalemate. No Score.`);
+  } else if (humanSelection === computerSelection) {
+    result.textContent = `${humanSelection.toUpperCase()} and ${computerSelection.toUpperCase()} - it's a stalemate. No Score.`;
   } else {
-    console.log(`${humanChoice} beats ${computerChoice}! You Win!`);
+    result.textContent = `${humanSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}! You Win!`;
     humanScore += 1;
   }
 };
 
-// Play a full game of 5 rounds
-const numberRounds = 5;
-const playGame = () => {
-  for (let i = 0; i < numberRounds; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-  }
-  if (humanScore > computerScore) {
-    console.log(`You Win the Game!\nScore: ${humanScore}\nComputer Score: ${computerScore}`);
-  } else if (humanScore < computerScore) {
-    console.log(`You lost. Computer Wins.\nScore: ${humanScore}\nComputer Score: ${computerScore}`);
-  } else {
-    console.log(`Tie Game.\n Score: ${humanScore}\nComputer Score: ${computerScore}`);
-  }
+// Add event listeners for buttons
+rockButton.addEventListener("click", () => {
+  const computerSelection = getComputerChoice();
+  const humanSelection = "rock";
+  playRound(humanSelection, computerSelection);
+});
+paperButton.addEventListener("click", () => {
+  const computerSelection = getComputerChoice();
+  const humanSelection = "paper";
+  playRound(humanSelection, computerSelection);
+});
+scissorButton.addEventListener("click", () => {
+  const computerSelection = getComputerChoice();
+  const humanSelection = "scissor";
+  playRound(humanSelection, computerSelection);
+});
+
+const showScore = () => {
+  currentScore.innerText = `Your Score: ${humanScore}\nComputer Score: ${computerScore}`;
+  resultDiv.appendChild(currentScore);
 };
+// // Play a full game of 5 rounds
+// const numberRounds = 5;
+// const playGame = () => {
+//   for (let i = 0; i < numberRounds; i++) {
+//     const humanSelection = getHumanChoice();
+//     const computerSelection = getComputerChoice();
+//     playRound(humanSelection, computerSelection);
+//   }
+//   if (humanScore > computerScore) {
+//     console.log(`You Win the Game!\nScore: ${humanScore}\nComputer Score: ${computerScore}`);
+//   } else if (humanScore < computerScore) {
+//     console.log(`You lost. Computer Wins.\nScore: ${humanScore}\nComputer Score: ${computerScore}`);
+//   } else {
+//     console.log(`Tie Game.\n Score: ${humanScore}\nComputer Score: ${computerScore}`);
+//   }
+// };
 
 // Initialize Game
-playGame();
+// playGame();
